34f94e30ea80635811e863b4388aec8805f546aa
The85thLetter <32557035+The85thLetter@users.noreply.github.com>
2018-03-15 16:52:46 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -144,11 +144,14 @@ function updateMice() {
         } else {
             useSpace = useSpace + 10;    
         }
-        if (temp<30 || temp>32) {
+        if (30<=temp>=32) {
+            if ((Math.floor(Math.random() * 2) + 1) <= 1) {
+                miceHealth[i] = miceHealth[i] + 5;
+            }
+        } else {
             if ((Math.floor(Math.random() * 2) + 1) <= 1) {
                 miceHealth[i] = miceHealth[i] - 5;
             }
-        } else {
         }
         
         if (miceHealth[i] <= 0) {
